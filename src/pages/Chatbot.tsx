2b27ed29603a8ae7250f82b8fc5de5f2250bd28f
gboksm11 <gaith.boksmati@gmail.com>
2024-10-09 09:59:58 +0300

    pre leave

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -162,6 +162,7 @@ const ChatBot: React.FC<ChatBotProps> = ({ activeChat, onFirstPrompt, isMobile, 
         }
     } else {
         setMessages([]);
+        setLastMessageIsLoading(false)
     }
 
   }, [activeChat]);
